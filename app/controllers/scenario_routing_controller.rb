7d5a8a5bb811623ffa34da4705b81aa35a4f2518
Sean Devine <barelyknown@icloud.com>
2014-10-11 15:07:01 -0700

    add other logic into scenario routing

--- app/controllers/scenario_routing_controller.rb
+++ app/controllers/scenario_routing_controller.rb
@@ -4,13 +4,29 @@ class ScenarioRoutingController < ApplicationController
   def create
     @incoming_call = IncomingCall.find(params[:incoming_call_id])
 
+    digits = params["Digits"]
+    scenarios = @incoming_call.practice_phone_number.scenarios.rank(:sequence).to_a
+    valid_digits = (1..(scenarios.count)).to_a
+
     response = Twilio::TwiML::Response.new do |r|
+
+      if digits.present?
+        if !valid_digits.include?(digits)
+          r.Say "#{digits} is not a valid choice. Please try again."
+        else
+          r.Say "You selected #{digits}, #{scenarios[digits - 1].name}."
+          r.Say "Redirecting"
+          # redirect
+        end
+      end
+
       r.Say "What scenario would you like to practice?"
       r.Pause length: 1
       r.Gather timeout: 10, numDigits: 1, method: "POST" do
         3.times do
-          @incoming_call.practice_phone_number.scenarios.rank(:sequence).each_with_index do |scenario, index|
+          scenarios.each_with_index do |scenario, index|
             r.Say "Press #{index + 1} for #{scenario.name}."
+            r.Pause length: 1
           end
         end
       end
