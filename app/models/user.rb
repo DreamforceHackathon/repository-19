560642de2920bd020c3b1c2017ad061d286381a5
Sean Devine <barelyknown@icloud.com>
2014-10-11 06:02:59 -0700

    remove password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   validates :phone_number, presence: true
   validates :phone_number, phone_number: true, if: -> (u) { u.phone_number.present? }
+  validates :phone_number, uniqueness: true
+  validates :password, presence: true, if: -> (u) { u.encrypted_password.blank? }
 
   def send_confirmation_notification?
     confirmation_required? && !@skip_confirmation_notification && self.phone_number.present?
