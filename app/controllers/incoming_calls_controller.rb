cc67e4ad6db9ab7c3754e6d6a2ac20e070ce042b
Sean Devine <barelyknown@icloud.com>
2014-10-11 14:31:29 -0700

    refactor the routing

--- app/controllers/incoming_calls_controller.rb
+++ app/controllers/incoming_calls_controller.rb
@@ -4,32 +4,16 @@ class IncomingCallsController < ApplicationController
   def create
     @practice_phone_number = PracticePhoneNumber.find(params[:practice_phone_number_id])
     @user = @practice_phone_number.users.find_by(phone_number: params["From"])
-
-    if @user.nil?
-      response = Twilio::TwiML::Response.new do |r|
+    response = Twilio::TwiML::Response.new do |r|
+      r.Play ActionController::Base.helpers.asset_url("cash_register.mp3")
+      if @user.nil?
         r.Say "You are not authorized to call for practice on this number. Goodbye."
+      else
+        @incoming_call = @practice_phone_number.incoming_calls.create!(twilio_sid: params["CallSid"], user: @user)
+        r.Redirect incoming_call_purpose_routing_path(@incoming_call), method: "POST"
       end
-
-      render xml: response.text and return
-    else
-      @incoming_call = @practice_phone_number.incoming_calls.create!(twilio_sid: params["CallSid"], user: @user)
-      response = Twilio::TwiML::Response.new do |r|
-        r.Play ActionController::Base.helpers.asset_url("cash_register.mp3")
-        r.Gather timeout: 10, numDigits: 1, action: incoming_call_purpose_routing_path(@incoming_call), method: "POST" do
-          r.Say "What would you like to do?"
-          3.times do
-            r.Say "Press 1 to practice."
-            r.Say "Press 2 to provide feedback."
-            r.Pause length: 1
-          end
-        end
-      end
-
-      puts response.text
-
-      render xml: response.text and return
     end
-
+    render xml: response.text
   end
 
 end
--- app/controllers/purpose_routing_controller.rb
+++ app/controllers/purpose_routing_controller.rb
@@ -3,11 +3,29 @@ class PurposeRoutingController < ApplicationController
 
   def create
     @incoming_call = IncomingCall.find(params[:incoming_call_id])
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say "You pressed #{params["Digits"]}."
-      r.Say "I found incoming call number #{@incoming_call.id}."
+
+    digits = params["Digits"]
+
+    if digits.blank?
+      response = Twilio::TwiML::Response.new do |r|
+        r.Play ActionController::Base.helpers.asset_url("cash_register.mp3")
+        r.Gather timeout: 10, numDigits: 1, method: "POST" do
+          r.Say "What would you like to do?"
+          3.times do
+            r.Say "Press 1 to practice."
+            r.Say "Press 2 to provide feedback."
+            r.Pause length: 1
+          end
+        end
+      end
+      render xml: response.text and return
+    else
+      response = Twilio::TwiML::Response.new do |r|
+        r.Say "You pressed #{params["Digits"]}."
+        r.Say "I found incoming call number #{@incoming_call.id}."
+      end
+      render xml: response.text and return
     end
-    render xml: response.text
   end
 
 end
