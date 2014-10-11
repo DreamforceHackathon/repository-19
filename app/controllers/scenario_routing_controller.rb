6bd942ffed73fd53a0cb492167d8c57e80bff869
Sean Devine <barelyknown@icloud.com>
2014-10-11 15:28:45 -0700

    rank the prompts

--- app/controllers/scenario_routing_controller.rb
+++ app/controllers/scenario_routing_controller.rb
@@ -17,7 +17,7 @@ class ScenarioRoutingController < ApplicationController
           scenario = scenarios[digits.to_i - 1]
           r.Say "You selected #{digits}, #{scenario.name}."
           r.Say "Record after the beeps. When you're done with each recording, press hashtag."
-          prompt = scenario.rank(:sequence).first
+          prompt = scenario.prompts.rank(:sequence).first
           r.Redirect incoming_call_prompt_pre_recording_path(@incoming_call, prompt_id: prompt.id)
         end
       end
