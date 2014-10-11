6ed3e0025178fd939a3d9585bec52693e6be1281
Sean Devine <barelyknown@icloud.com>
2014-10-11 13:51:37 -0700

    add initial prompt menu

--- app/controllers/incoming_calls_controller.rb
+++ app/controllers/incoming_calls_controller.rb
@@ -15,7 +15,14 @@ class IncomingCallsController < ApplicationController
       @incoming_call = @practice_phone_number.incoming_calls.new(twilio_sid: params["CallSid"])
       response = Twilio::TwiML::Response.new do |r|
         r.Say "Beep. Beep. Beep."
-        r.Say "What would you like to do?"
+        r.Gather timeout: 60, numDigits: 1, method: "GET" do
+          r.Say "What would you like to do?"
+          3.times do
+            r.Say "Press 1 to practice."
+            r.Say "Press 2 to provide feedback."
+            r.Pause length: 1
+          end
+        end
       end
 
       render xml: response.text and return
