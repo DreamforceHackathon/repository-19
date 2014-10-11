ff3a7770f396c17e4a20e828b70151911fcc98ef
Sean Devine <barelyknown@icloud.com>
2014-10-11 15:58:06 -0700

    tighten up the audio

--- app/controllers/prompt_pre_recording_controller.rb
+++ app/controllers/prompt_pre_recording_controller.rb
@@ -5,7 +5,10 @@ class PromptPreRecordingController < ApplicationController
     @incoming_call = IncomingCall.find(params[:incoming_call_id])
     @prompt = Prompt.find(params[:prompt_id])
 
+    prompt_sequence = @prompt.scenario.ranks(:prompts).pluck(:id).index(@prompt.id) + 1
+
     response = Twilio::TwiML::Response.new do |r|
+      r.Say "Question number #{prompt_sequence}."
       r.Say @prompt.content
       r.Pause length: 2
       r.Redirect incoming_call_recordings_path(@incoming_call, recordable_type: @prompt.class.to_s, recordable_id: @prompt.id)
