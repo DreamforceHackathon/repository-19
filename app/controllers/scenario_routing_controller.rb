62694bb8ad260d76de635c90cc73e977c0262bae
Sean Devine <barelyknown@icloud.com>
2014-10-11 16:47:15 -0700

    switch to star

--- app/controllers/scenario_routing_controller.rb
+++ app/controllers/scenario_routing_controller.rb
@@ -15,7 +15,7 @@ class ScenarioRoutingController < ApplicationController
           r.Say "#{digits} is not a valid choice. Please try again."
         else
           scenario = scenarios[digits.to_i - 1]
-          r.Say "Reekord after the beeps. When you're done with each recording, press hashtag."
+          r.Say "Reekord after the beeps. When you're done with each recording, press star."
           prompt = scenario.prompts.rank(:sequence).first
           r.Pause 2
           r.Redirect incoming_call_prompt_pre_recording_path(@incoming_call, prompt_id: prompt.id)
