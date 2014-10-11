f5206482bd017f307cbbc61ab85056a0c95ec75a
Sean Devine <barelyknown@icloud.com>
2014-10-11 14:22:48 -0700

    finally figured out what it was - csrf

--- app/controllers/purpose_routing_controller.rb
+++ app/controllers/purpose_routing_controller.rb
@@ -1,4 +1,5 @@
 class PurposeRoutingController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def create
     @incoming_call = IncomingCall.find(params[:incoming_call_id])
