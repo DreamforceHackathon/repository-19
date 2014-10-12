2f5e874bb2e8d6999756ce833a7d2a6f746babd5
Sean Devine <barelyknown@icloud.com>
2014-10-12 10:49:46 -0700

    dependent destroy responses

--- app/models/incoming_call.rb
+++ app/models/incoming_call.rb
@@ -2,7 +2,7 @@ class IncomingCall < ActiveRecord::Base
 
   belongs_to :practice_phone_number
   belongs_to :user
-  has_many :responses
+  has_many :responses, dependent: :destroy
 
   validates :practice_phone_number, presence: true
   validates :user, presence: true
