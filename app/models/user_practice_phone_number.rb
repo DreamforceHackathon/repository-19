1e0bfeba3604629ac333ed464135f11896f70f9c
Sean Devine <barelyknown@icloud.com>
2014-10-11 20:10:52 -0700

    add txt message invite

--- app/models/user_practice_phone_number.rb
+++ app/models/user_practice_phone_number.rb
@@ -9,4 +9,20 @@ class UserPracticePhoneNumber < ActiveRecord::Base
   validates :practice_phone_number, presence: true
   validates :user_id, uniqueness: { scope: :practice_phone_number_id }
 
+  after_commit :send_txt_message_invite, on: [:create]
+
+  def send_txt_message_invite
+    twilio_client.messages.create(
+      from: practice_phone_number.phone_number,
+      to: user.phone_number,
+      body: "You've been invited to Call for Practice. Just call back this number to get started."
+    )
+  end
+
+private
+
+  def twilio_client
+    @twilio_client ||= Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
+  end
+
 end
