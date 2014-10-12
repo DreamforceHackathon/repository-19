f4e8c44b8c803d6ef8ef512efc075e12aeccee34
Sean Devine <barelyknown@icloud.com>
2014-10-12 02:41:53 -0700

    add recording method to response

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,6 +2,7 @@ class Response < ActiveRecord::Base
 
   belongs_to :prompt
   belongs_to :incoming_call
+  has_one :recording, as: :recordable
 
   validates :prompt, presence: true
   validates :incoming_call, presence: true
--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe Response do
   it { is_expected.to belong_to :incoming_call }
   it { is_expected.to validate_presence_of :incoming_call }
 
+  it { is_expected.to have_one(:recording) }
+
 end
