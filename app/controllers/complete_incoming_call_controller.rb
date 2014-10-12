2e2c2e1f2f36e8652ff2fc4a00919abedf10bd58
Sean Devine <barelyknown@icloud.com>
2014-10-12 04:50:07 -0700

    guard against a incoming call not being there

--- app/controllers/complete_incoming_call_controller.rb
+++ app/controllers/complete_incoming_call_controller.rb
@@ -2,15 +2,16 @@ class CompleteIncomingCallController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @incoming_call = IncomingCall.find_by(twilio_sid: params["CallSid"])
-    responses = @incoming_call.responses.to_a
-    recorded_response_count = responses.select { |r| r.recording.url.present? }.count
-    if recorded_response_count > 0
-      twilio_client.messages.create(
-        from: @incoming_call.practice_phone_number.phone_number,
-        to: @incoming_call.user.phone_number,
-        body: "You recorded #{view_context.pluralize(recorded_response_count, "response")} during this practice session. #{incoming_call_url(@incoming_call, s: @incoming_call.user.signature)}"
-      )
+    if @incoming_call = IncomingCall.find_by(twilio_sid: params["CallSid"])
+      responses = @incoming_call.responses.to_a
+      recorded_response_count = responses.select { |r| r.recording.url.present? }.count
+      if recorded_response_count > 0
+        twilio_client.messages.create(
+          from: @incoming_call.practice_phone_number.phone_number,
+          to: @incoming_call.user.phone_number,
+          body: "You recorded #{view_context.pluralize(recorded_response_count, "response")} during this practice session. #{incoming_call_url(@incoming_call, s: @incoming_call.user.signature)}"
+        )
+      end
     end
     head :ok
   end
