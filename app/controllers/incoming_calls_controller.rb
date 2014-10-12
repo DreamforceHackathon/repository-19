4f42c237b0e676e788e5934f9037ba3e77325ebb
Sean Devine <barelyknown@icloud.com>
2014-10-12 05:07:31 -0700

    handle potential param problems in incoming calls controller

--- app/controllers/incoming_calls_controller.rb
+++ app/controllers/incoming_calls_controller.rb
@@ -12,12 +12,15 @@ class IncomingCallsController < ApplicationController
   end
 
   def create
-    @practice_phone_number = PracticePhoneNumber.find(params[:practice_phone_number_id])
+    @practice_phone_number = PracticePhoneNumber.find_by(id: params[:practice_phone_number_id])
     @user = @practice_phone_number.users.find_by(phone_number: params["From"])
+
     response = Twilio::TwiML::Response.new do |r|
       r.Play ActionController::Base.helpers.asset_url("cash_register.mp3")
       if @user.nil?
         r.Say "You are not authorized to call for practice on this number. Goodbye."
+      elsif @practice_phone_number.nil?
+        r.Say "The request was invalid. Goodbye."
       else
         @incoming_call = @practice_phone_number.incoming_calls.create!(twilio_sid: params["CallSid"], user: @user)
         r.Redirect incoming_call_scenario_routing_path(@incoming_call), method: "POST"
